4b20846fd41448039fbaf2c3eea081124a7bdf8e
Pedro Araújo Marcato <pedro.marcato@iesb.edu.br>
2025-10-17 10:45:41 -0300

    atualizacoes Aula10

--- aulas/aula10/app.js
+++ aulas/aula10/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const tarefasRouter = require('./routes/tarefasRouter')
 
-const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/`;
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DBNAME}`;
 
 mongoose
     .connect(url)
