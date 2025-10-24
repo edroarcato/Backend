3d8d121530b1e63d631c4460b9f3c88b6aaef15e
Pedro Araújo Marcato <pedro.marcato@iesb.edu.br>
2025-10-24 09:50:17 -0300

    pratica07 corrigido

--- praticas/pratica07/app.js
+++ praticas/pratica07/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const produtosRouter = require('./routes/produtosRouter');
 
-const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DBNAME}`;
+const url = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`;
 
 mongoose
     .connect(url)
