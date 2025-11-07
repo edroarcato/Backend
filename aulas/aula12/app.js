3e975b44130484ffed1de6b5d268c8fc8d019de9
Pedro Araújo Marcato <pedro.marcato@iesb.edu.br>
2025-11-07 10:45:35 -0300

    Swagger aula12

--- aulas/aula12/app.js
+++ aulas/aula12/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const tarefasRouter = require("./routes/tarefaRouter");
 const apidocsRouter = require('./routes/apidocs');
 
 const app = express();
@@ -10,7 +10,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+app.use("/tarefas", tarefasRouter);
 app.use("/api-docs", apidocsRouter);
 
 module.exports = app;
